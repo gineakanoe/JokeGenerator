397eb44e4ab01730108207530f7f5894655681a1
Ginea Merrill <gineakanoe@gmail.com>
2021-09-28 16:59:56 -0400

    added files to jokes folder

---
+++ app.js
@@ -0,0 +1,32 @@
+//* Used https://www.jokeapi.dev/ for API database
+//* Base code came from YouTuber DThompsonDev; video link is https://www.youtube.com/watch?v=6WclW4ThDUU&ab_channel=DThompsonDev
+
+
+
+const jokeText = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJokes)
+
+generateJokes()
+
+async function generateJokes(){
+    const res = await fetch(
+        "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+    )
+    const data = await res.json();
+    console.log(data);
+    console.log(data.joke);
+    console.log(data.delivery);
+
+    let joke = ""
+
+    if (data.joke == undefined) {
+        joke = `${data.setup} <br/> ${data.delivery}`
+    } else if(data.delivery == undefined) {
+        joke = data.joke
+    }else {
+        return 'This is not the joke you are looking for'
+    }
+    jokeText.innerHTML = joke
+}
